1d794366f56a5e54b3da03f6231555324e2588e6
Dave Sarbora <dsarbora@gmail.com>
2019-01-14 19:29:22 -0800

    added customizeable background colors

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(function(){
+  $("#green").click(function() {
+    $("#index").removeClass();
+    $("#index").addClass("green-background");
+  });
+  $("#red").click(function() {
+    $("#index").removeClass();
+    $("#index").addClass("red-background");
+  });
+  $("#blue").click(function() {
+    $("#index").removeClass();
+    $("#index").addClass("blue-background");
+  });
+  $("#grey").click(function() {
+    $("#index").removeClass();
+    $("#index").addClass("grey-background");
+  });
+  $("#purple").click(function() {
+    $("#index").removeClass();
+    $("#index").addClass("purple-background");
+  });
+  $("#white").click(function() {
+    $("#index").removeClass();
+    $("#index").addClass("white-background");
+  });
+});
